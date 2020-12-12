f8a765874ed4ca175b1e2d2935f754fe0282a265
Toccara Hicks <toccarahicks11@gmail.com>
2020-12-12 08:17:24 -0600

    trying to get api to work

--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -2,12 +2,16 @@ const path = require("path");
 const router = require("express").Router();
 
 router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
-
-
-router.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-module.exports = router;
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = (router) => {
+  router.get("/notes", (req, res) =>{
+    res.sendFile(path.join(__dirname + "/../public", "notes.html"));
+  });
+};
