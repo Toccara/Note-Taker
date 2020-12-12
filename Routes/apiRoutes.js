b731b0d30b3482936f67f33c88b7af6da713fd8b
Toccara Hicks <toccarahicks11@gmail.com>
2020-12-12 07:06:44 -0600

    updated api routes

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,59 +1,25 @@
-const fs = require("fs");
-module.exports = (app) => {
-    //request and response from notes db.json file
-    app.get("/api/notes", (req, res) => {
-        //read the db.json file to get all notes
-        fs.readFile(__dirname + "/../db/db.json", (err, data) => {
-            if (err) throw err;
-            const notes = JSON.parse(data);
-            //parsed notes that user will see
-            return res.json(notes);
-        });
-    });
+const router = require("express").Router();
+const store = require("../db/store");
 
-    app.post("/api/notes", (req, res) => {
-        const newNote = req.body;
-        let allNotes = [];
-        let id = 0;
-        fs.readFile(__dirname + "/../db/db.json", "utf8", (err, data) => {
-            if (err) throw err;
-            allNotes = JSON.parse(data);
-            for (let i = 0; i < allNotes.length; i++) {
-                if (allNotes[i].id > id) {
-                    id = allNotes[i].id;
-                }
-            }
-            newNote.id = parseInt(id) + 1;
-            allNotes.push(newNote);
-            fs.writeFile(
-                __dirname + "/../db/db.json",
-                JSON.stringify(allNotes),
-                "utf8",
-                (err) => {
-                    if (err) throw err;
-                    res.end();
-                }
-            );
-        });
-    });
+router.get("/notes", (req, res) => {
+  store
+    .getNotes()
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+});
 
-    app.delete("/api/notes/:id", (req, res) => {
-        const deleteID = req.params.id;
-        fs.readFile(__dirname + "/../db/db.json", "utf8", (err, data) => {
-            if (err) throw err;
-            const deleteNote = JSON.parse(data).filter((entry) => {
-                return entry.id != deleteID;
-            });
-            fs.writeFile(
-                __dirname + "/../db/db.json",
-                JSON.stringify(deleteNote),
-                "utf8",
-                (err) => {
-                    
-                    if (err) throw err;
-                    res.end();
-                }
-            );
-        });
-    });
-};
+router.post("/notes", (req, res) => {
+  store
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
+});
+
+router.delete("/notes/:id", (req, res) => {
+  store
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
