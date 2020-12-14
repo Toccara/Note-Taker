8427afd2a050d13ae7f7f518f2e7839c33121c25
Toccara Hicks <toccarahicks11@gmail.com>
2020-12-14 10:50:20 -0600

    trying to buttons to work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,31 +2,9 @@ const fs = require("fs");
 const router = require("express").Router();
 const store = require("../db/store");
 
-router.get("/notes", (req, res) => {
-    store
-        .getNotes()
-        .then((notes) => res.json(notes))
-        .catch((err) => res.status(500).json(err));
-});
-
-router.post("/notes", (req, res) => {
-    store
-        .addNote(req.body)
-        //fs.readFile(__dirname + "/../db/db.json", "utf8", (err, data) =>
-        .then((note) => res.json(note))
-        .catch((err) => res.status(500).json(err));
-});
-
-router.delete("/notes/:id", (req, res) => {
-    store
-        .removeNote(req.params.id)
-        .then(() => res.json({ ok: true }))
-        .catch((err) => res.status(500).json(err));
-});
-
 module.exports = function (router) {
 
-    router.get("/api/notes", (req, res) => {
+    router.get("../public/notes", (req, res) => {
         //read the db.json file to get all notes
         fs.readFile(__dirname + "/../db/db.json", (err, data) => {
             if (err)
@@ -35,6 +13,30 @@ module.exports = function (router) {
             return res.json(notes);
         });
     });
+
+router.get("../public/notes", (req, res) => {
+    store
+        .getNotes()
+        .then((notes) => res.json(notes))
+        .catch((err) => res.status(500).json(err));
+});
+
+router.post("../notes", (req, res) => {
+    store
+        .addNote(req.body)
+        //fs.readFile(__dirname + "/../db/db.json", "utf8", (err, data) =>
+        .then((note) => res.json(note))
+        .catch((err) => res.status(500).json(err));
+});
+
+router.delete("../notes/:id", (req, res) => {
+    store
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch((err) => res.status(500).json(err));
+});
+
+
 }
 //fs.writeFile(
     //__dirname + "/../db/db.json",
